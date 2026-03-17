2d956630dc0f40e2ab244c7e4129bb324dc3b012
Amorn Phanturat <amornp@Amorns-MacBook-Air.local>
2026-03-17 17:39:53 +0700

    feat: add travesal solver (not yet handle edge cases)

---
+++ graph.h
@@ -0,0 +1,140 @@
+#include <iostream>
+// #include <omp.h>
+#include <vector>
+#include <bitset>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+namespace MVC
+{
+
+    class Graph
+    {
+    public:
+        int n_vertices;
+        vector<vector<int> > adj;
+        vector<int> bestMVC;
+        int bestCost = 1e6;
+
+        Graph(int n) : bestMVC(n), adj(n), n_vertices(n) {};
+
+        void addEdge(int u, int v)
+        {
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        void calcMVC()
+        {
+            vector<int> picks;
+            vector<bool> coverage(this->n_vertices, false), visited(this->n_vertices, false);
+
+            pickZeroDegreeNode(coverage,picks);
+            
+            for(int i = 0; i < this->n_vertices ; i++) {
+                recursiveMVC(picks, coverage, visited, i , -1);
+            }
+        }
+
+        bool isFullCoverage(vector<bool> &coverage) {
+            for(bool x : coverage) if(!x) return false;
+            return true;
+        }
+
+        void pickZeroDegreeNode(vector<bool> &coverage,vector<int> &picks) {
+            int i = 0;
+            for(auto x : this->adj) {
+                if(x.size() == 0) {
+                    coverage[i] = 1;
+                    picks.push_back(i);
+                }
+                i++;
+            }
+        }
+
+        void recursiveMVC(vector<int> &picks, vector<bool> &coverage, vector<bool> &visited, int currVertex , int origin ) {
+
+            if(picks.size() > bestCost) {
+
+                // BEGIN : DEBUG
+                cout<<"The result was bounded, Terminating this flow\n";
+                // END : DEBUG
+
+                return;
+            }
+
+            // BEGIN : DEBUG
+            cout<<"currVertex "<<currVertex<<" parent "<<origin<<"\n";
+            for(auto y : coverage) cout<<y<<" ";
+            cout<<"\n";
+            // END : DEBUG
+
+            if (isFullCoverage(coverage)) {
+
+                // BEGIN : DEBUG
+                cout<<"Got full coverage\n";
+                // END : DEBUG
+                
+                if (picks.size() < this->bestCost) {
+                    this->bestMVC = picks;
+                    this->bestCost = picks.size();
+                }
+                return;
+            }
+            
+            if(visited[currVertex]) {
+
+                // BEGIN : DEBUG
+                cout<<"Hit deadend\n";
+                // END : DEBUG
+
+                return;
+            }
+
+            visited[currVertex] = true;
+
+            vector<bool> temp_coverage = coverage;
+
+            for(auto x : this->adj[currVertex]) {
+            
+                // Route 1 : don't pick currVertex
+
+                // BEGIN : DEBUG
+                cout<<"currVertex "<<currVertex<<" parent "<<origin<<"\n";
+                cout<<"B2 : not picking "<<currVertex<<"\n";
+                cout<<"Goto "<<x<<"\n";
+                for(auto y : coverage) cout<<y<<" ";
+                cout<<"\n";
+                // END : DEBUG
+
+                recursiveMVC(picks,coverage,visited,x,currVertex);
+                coverage = temp_coverage;
+
+                // Route 2 : pick currVertex
+
+                // BEGIN : DEBUG
+                cout<<"currVertex "<<currVertex<<" parent "<<origin<<"\n";
+                cout<<"B1 : pick "<<currVertex<<"\n";
+                cout<<"Goto "<<x<<"\n";
+                // END : DEBUG
+
+                picks.push_back(currVertex);
+                coverage[currVertex] = true;
+                for(auto neighbour : this->adj[currVertex]) coverage[neighbour] = true;
+                
+                // BEGIN : DEBUG
+                for(auto y : coverage) cout<<y<<" ";
+                cout<<"\n";
+                // END : DEBUG
+                recursiveMVC(picks,coverage,visited,x,currVertex);
+                coverage = temp_coverage;
+                picks.pop_back();
+
+            }
+
+            visited[currVertex] = false;
+        }
+    };
+}
--- main.cpp
+++ main.cpp
@@ -1,34 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <omp.h>
+// #include <omp.h>
+#include "graph.h"
 
 using namespace std;
+using namespace MVC;
 
-int max_threads = omp_get_max_threads();
-
-class Graph {
-    public :
-        int n_vertices;
-        vector<vector<int>> adj;
-        vector<pair<int,int>> edges;
-        Graph(int n) : adj(n) , n_vertices(n) {};
-        void addEdge(int u , int v) {
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-            edges.push_back(make_pair(u,v));
-        }
-};
+// int max_threads = omp_get_max_threads();
 
 int main(int args , char** kwargs) {
-    int n , v, e;
-    printf("Running with %d threads",max_threads);
-    cin>>n>>v>>e;
-    vector<int> adj[n];
+    int v, e;
+    // printf("Running with %d threads",max_threads);
+    cin>>v>>e;
+    Graph g = Graph(v);
     for(int i = 0 ; i < e ; i++) {
         int f, t;
         cin>>f>>t;
-        adj[f].push_back(t);
-        adj[t].push_back(f);
+        g.addEdge(f,t);
     }
+    g.calcMVC();
+    cout<<"Min cost: "<<g.bestCost<<"\nConfiguration: ";
+    for(auto x : g.bestMVC) cout<<x<<" ";
+    cout<<"\n";
     return 0;
 }
